f86dc751f278bdf8e32f96e460e1446459bf4ed9
Merajalam2 <merajalam7903@gmail.com>
2026-06-30 02:55:48 +0530

    cors updated for vercel

--- backend/index.js
+++ backend/index.js
@@ -16,23 +16,25 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
+
 const corsOptions = {
-    origin:'http://localhost:5174',
-    credentials:true
+    origin: [
+        'http://localhost:5174',
+        'http://localhost:5173',
+        'https://job-portal-frontend-xi-murex.vercel.app'
+    ],
+    credentials: true
 }
 
 app.use(cors(corsOptions));
 
 const PORT = process.env.PORT || 3000;
 
-
 // api's
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
-
-
 
 app.listen(PORT,()=>{
     connectDB();
